45cf78bf2e4cda7a0faf8bfe58ae11cf18d34fa1
leonhma <67312077+leonhma@users.noreply.github.com>
2021-05-09 18:15:56 +0200

    better docs?

--- mebispy/__init__.py
+++ mebispy/__init__.py
@@ -8,14 +8,14 @@ from .common.exceptions import LoginError, HTTPError, ActionFailedError
 
 
 class UserSession():
-    """Class representing the session of an logged-in user.
+    """Class representing the session of a logged-in user.
 
     Args:
-        user (str, required) The username used to sign in.
-        pwd (str, required) The password used to sign in.
+       user (str, required): The username used to sign in.
+       pwd (str, required): The password used to sign in.
 
     Attributes:
-        sesskey: (str) The session key. (Made accessible for advanced users)
+       sesskey The session key. (Made accessible for advanced users)
     """
 
     def __init__(self, user: str, pwd: str):
@@ -47,13 +47,13 @@ class UserSession():
 
     def get(self, *args, **kwargs):
         """Make a GET request in the context of the user's session.
-            (Made accessible for advanced users.)
+           (Made accessible for advanced users.)
 
         Raises:
-            HTTPError: If the request was answered with an error.
+           `HTTPError`: If the request was answered with an error.
 
         Note: This a wrapper around :func:`requests.get`.
-            [docs here](https://docs.python-requests.org/en/master/api/)
+           [docs here](https://docs.python-requests.org/en/master/api/)
         """
         r = self._session.get(*args, **kwargs)
         if r.status_code >= 400:
@@ -62,13 +62,13 @@ class UserSession():
 
     def post(self, *args, **kwargs):
         """Make a POST request in the context of the user's session.
-            (Made accessible for advanced users.)
+           (Made accessible for advanced users.)
 
         Raises:
-            HTTPError: If the request was answered with an error.
+           `HTTPError`: If the request was answered with an error.
 
         Note: This a wrapper around :func:`requests.post`.
-            [docs here](https://docs.python-requests.org/en/master/api/)
+           [docs here](https://docs.python-requests.org/en/master/api/)
         """
         r = self._session.post(*args, **kwargs)
         if r.status_code >= 400:
@@ -76,15 +76,18 @@ class UserSession():
         return r
 
     def ajax(self, method: str, args: dict) -> dict:
-        """Make an request to the ajax endpoint of mebis.
-            (Made accessible for advanced users.)
+        """Make a request to the ajax endpoint of mebis.
+           (Made accessible for advanced users.)
 
         Args:
-            method (str, required): The identifier of the method.
-            args (dict, required): The arguments to the method.
+           method (str, required): The identifier of the method.
+           args (dict, required): The arguments to the method.
+
+        Raises:
+           ActionFailedError: If the response indicates an error.
 
         Returns:
-            Response: The reponse of the request.
+           dict: The reponse data of the request in json form.
         """
         # TODO add documentation
         r = self.post(
@@ -97,31 +100,24 @@ class UserSession():
                 + ' look at the docs.')
         return r['data']
 
-    class Helpers():
-        def __init__(self, sess):
-            self.post = sess.post
-            self.get = sess.get
-            self.ajax = sess.ajax
-            self.sesskey = sess.sesskey
+    def make_survey_choice(self,
+                           survey_id: Union[int, str],
+                           choice_id: Union[int, str]) -> bool:
+        """Helper for making survey choices.
 
-        def make_survey_choice(self,
-                               survey_id: Union[int, str],
-                               choice_id: Union[int, str]) -> bool:
-            """Helper for making survey choices.
+        Args:
+           survey_id (str | int, required) The id of the survey.
+              (Can be found in the url when looking at the survey.)
+           choice_id (str | int, required) The id of your choice.
+              (Can be found through the devtools inspector.)
 
-            Args:
-                survey_id (str | int, required) The id of the survey.
-                    (Can be found in the url when looking at the survey.)
-                choice_id (str | int, required) The id of your choice.
-                    (Can be found through the devtools inspector.)
+        Returns:
+           bool: True if choice was succesfully set, False otherwise
+        """
 
-            Returns:
-                bool: True if choice was succesfully set, False otherwise
-            """
-
-            r = self.post('https://lernplattform.mebis.bayern.de/'
-                          + 'mod/choice/view.php',
-                          {'answer': choice_id, 'sesskey': self.sesskey,
-                           'action': 'makechoice', 'id': survey_id},
-                          allow_redirects=False)
-            return True if 'location' in r.headers else False
+        r = self._post('https://lernplattform.mebis.bayern.de/'
+                       + 'mod/choice/view.php',
+                       {'answer': choice_id, 'sesskey': self._sesskey,
+                        'action': 'makechoice', 'id': survey_id},
+                       allow_redirects=False)
+        return True if 'location' in r.headers else False
