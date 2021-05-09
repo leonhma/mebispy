f45eaea4f29d43cb9503a07e758d0c9507bb58de
leonhma <67312077+leonhma@users.noreply.github.com>
2021-05-09 10:37:17 +0200

    added basic auth and request functions

--- mebispy/__init__.py
+++ mebispy/__init__.py
@@ -1,9 +1,10 @@
-# pylama:ignore=E501
 from html import unescape
-from requests import Session
+from json import loads
 from re import search
 
-from .common.exceptions import LoginError, ActionFailedError
+from requests import Session, Response
+
+from .common.exceptions import LoginError, HTTPError
 
 
 class UserSession():
@@ -17,23 +18,30 @@ class UserSession():
         sesskey: (str) The session key. (Made accessible for advanced users)
     """
 
-    def __init__(self, user, pwd):
+    def __init__(self, user: str, pwd: str):
         self._login(user, pwd)
+        self.helpers = self.Helpers(self)
 
     def _login(self, user, pwd):
         self._session = Session()
         # fill up cookie jar
-        r = self._session.get('https://lernplattform.mebis.bayern.de')
+        r = self.get('https://lernplattform.mebis.bayern.de')
         # sign in to get session tokens
-        nexturl = 'https://idp.mebis.bayern.de' + search(r'(?<=action=\").*?(?=\")', r.text).group(0)
-        r = self._session.post(nexturl, data={'j_username': user, 'j_password': pwd, '_eventId_proceed': ''})
+        nexturl = 'https://idp.mebis.bayern.de'\
+            + search(r'(?<=action=\").*?(?=\")', r.text).group(0)
+        r = self.post(nexturl, data={'j_username': user,
+                                     'j_password': pwd,
+                                     '_eventId_proceed': ''})
         if 'form-error' in r.text:
             raise LoginError(self._user)
         # complete full signin
         nexturl = unescape(search(r'(?<=action=\").*?(?=\")', r.text).group(0))
-        rs = unescape(search(r'(?<=name=\"RelayState\" value=\").*?(?=\")', r.text).group(0))
-        saml = search(r'(?<=name=\"SAMLResponse\" value=\").*?(?=\")', r.text).group(0)
-        r = self._session.post(nexturl, data={'RelayState': rs, 'SAMLResponse': saml})
+        rs = unescape(search(r'(?<=name=\"RelayState\" value=\").*?(?=\")',
+                             r.text).group(0))
+        saml = search(r'(?<=name=\"SAMLResponse\" value=\").*?(?=\")',
+                      r.text).group(0)
+        r = self.post(nexturl, data={'RelayState': rs,
+                                     'SAMLResponse': saml})
         # get sesskey
         self.sesskey = search(r'(?<=sesskey\"\:\").*?(?=\")', r.text).group(0)
 
@@ -41,32 +49,75 @@ class UserSession():
         """Make a GET request in the context of the user's session.
             (Made accessible for advanced users.)
 
-        Note: This a wrapper around :func:`requests.get` [documentation here](https://docs.python-requests.org/en/master/api/)
+        Raises:
+            HTTPError: If the request was answered with an error.
+
+        Note: This a wrapper around :func:`requests.get`.
+            [docs here](https://docs.python-requests.org/en/master/api/)
         """
         r = self._session.get(*args, **kwargs)
+        if r.status_code >= 400:
+            raise HTTPError(r)
         return r
 
     def post(self, *args, **kwargs):
         """Make a POST request in the context of the user's session.
             (Made accessible for advanced users.)
 
-        Note: This a wrapper around :func:`requests.post`. [documentation here](https://docs.python-requests.org/en/master/api/)
+        Raises:
+            HTTPError: If the request was answered with an error.
+
+        Note: This a wrapper around :func:`requests.post`.
+            [docs here](https://docs.python-requests.org/en/master/api/)
         """
         r = self._session.post(*args, **kwargs)
+        if r.status_code >= 400:
+            raise HTTPError(r)
         return r
 
-    def make_survey_choice(self, survey_id: int, choice_id: int):
-        """Helper for making survey choices.
+    def ajax(self, method: str, args: dict) -> Response:
+        """Make an request to the ajax endpoint of mebis.
+            (Made accessible for advanced users.)
 
         Args:
-            survey_id (str | int, required) The id of the survey.
-                (Can be found in the url when looking at the survey.)
-            choice_id (str | int, required) The id of your choice.
-                (Can be found through the devtools inspector.)
-        """
+            method (str, required): The identifier of the method.
+            args (dict, required): The arguments to the method.
 
-        r = self.post('https://lernplattform.mebis.bayern.de/mod/choice/view.php',
-                      {'answer': choice_id, 'sesskey': self.sesskey, 'action': 'makechoice', 'id': survey_id},
-                      allow_redirects=False)
-        if 'location' not in r.headers:
-            raise ActionFailedError('Choice couldn`t be set. It likely got disabled by an administrator or the wrong ids were given.')
+        Returns:
+            Response: The reponse of the request.
+        """
+        # TODO add documentation
+        r = self.post(
+            'https://lernplattform.mebis.bayern.de/lib/ajax/service.php',
+            params={'sesskey': self.sesskey},
+            json=[{"index": 0, "methodname": method, "args": args}])
+        return r.json()
+
+    class Helpers():
+        def __init__(self, sess):
+            self.post = sess.post
+            self.get = sess.get
+            self.ajax = sess.ajax
+            self.sesskey = sess.sesskey
+
+        def make_survey_choice(self,
+                               survey_id: int | str,
+                               choice_id: int | str) -> bool:
+            """Helper for making survey choices.
+
+            Args:
+                survey_id (str | int, required) The id of the survey.
+                    (Can be found in the url when looking at the survey.)
+                choice_id (str | int, required) The id of your choice.
+                    (Can be found through the devtools inspector.)
+
+            Returns:
+                bool: True if choice was succesfully set, False otherwise
+            """
+
+            r = self.post('https://lernplattform.mebis.bayern.de/'
+                          + 'mod/choice/view.php',
+                          {'answer': choice_id, 'sesskey': self.sesskey,
+                           'action': 'makechoice', 'id': survey_id},
+                          allow_redirects=False)
+            return True if 'location' in r.headers else False
